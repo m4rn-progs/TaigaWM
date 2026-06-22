ec91db183d44a175b8bc287d2f2b923710e72960
zoey <zoeyyy@noreply.codeberg.org>
2026-06-22 10:54:57 -0600

    Remove redundant comments and cleanup code

--- src/taiga.lua
+++ src/taiga.lua
@@ -13,11 +13,10 @@ local wm = require("wm")
 local globals = require("globals")
 local libinput = require("libinput")
 local seat = require("seat")
+
 -- required protocols
 wau:require("..include.protocol.river-xkb-bindings-v1")
 wau:require("..include.protocol.river-layer-shell-v1")
-
-
 
 -- ENTRY POINT --
 DISPLAY = wau.wl_display.connect()
@@ -31,11 +30,7 @@ local function handle_callback_error(proxy, name, func, err)
 end
 
 wau.wl_proxy.set_error_callback(handle_callback_error)
-
--- Avoid passing WAYLAND_DEBUG to our children
 posix.stdlib.setenv("WAYLAND_DEBUG", nil)
-
--- Ensure children are automatically reaped
 posix.signal.signal(posix.signal.SIGCHLD, posix.signal.SIG_IGN)
 
 -- Registry
@@ -59,44 +54,35 @@ registry:add_listener({
 
 DISPLAY:roundtrip()
 
--- assert that globals exist
 for k in pairs(globals.required_globals) do
 	assert(globals.globals[k] ~= nil, ("wayland compositor does not support %s"):format(k))
 end
 
--- add listeners
 globals.globals["river_window_manager_v1"]:add_listener(wm.wm_handlers)
--- had to add the if statement because if libinput fails (no mouse or smth) or the mouse thingy fails we can still boot the wm
-if globals.globals["river_libinput_config_v1"] then
-	globals.globals["river_libinput_config_v1"]:add_listener(libinput.libinput_handlers)
-end
+globals.globals["river_libinput_config_v1"]:add_listener(libinput.libinput_handlers)
 
 signal.signal(signal.SIGUSR1, function()
 	TAIGARC = config.open_config()
 	config.xkb_bindings = CONFIG_KEYBINDS().keyboard_binds or {}
 	config.pointer_bindings = CONFIG_KEYBINDS().mouse_binds or {}
 
-	-- reset everything
 	for _, seat_local in ipairs(wm.wm.seats) do
-		-- next time seat:manage is called, with seat.new = true it will re-set it up
         CONFIG_FILE_RELOAD = true
 		seat_local.new = true
-		-- reset bindings in each seat aswell
 		for _, binding in ipairs(seat.xkb_bindings) do
 			binding.obj:destroy()
 		end
 		for _, binding in ipairs(seat.pointer_bindings) do
 			binding.obj:destroy()
 		end
-		-- after destroying the objs, reset them to an empty table
 		seat_local.xkb_bindings = {}
 		seat_local.pointer_bindings = {}
 	end
 
-	-- start watching for changes again
 	config.watch_config_changes()
 end)
 
+-- init the config and start the compositor main loop
 config.init()
 
 while DISPLAY:dispatch() do
